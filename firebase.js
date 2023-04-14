51e9f7f4cfac3deacacc4d7832f0e058f1d7c223
gokrusk <nigelljama@gmail.com>
2023-04-14 11:10:21 -0500

    commits nigel

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPQLbbeqp2UCIEWzk8NamR4jMiWfhfELs",
+  authDomain: "app-concurso-c04d8.firebaseapp.com",
+  projectId: "app-concurso-c04d8",
+  storageBucket: "app-concurso-c04d8.appspot.com",
+  messagingSenderId: "1089095123404",
+  appId: "1:1089095123404:web:c6c53656b0066f23d3dd69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

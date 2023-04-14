a99f687ee4a6fc3946931a8a192a13b976c385f8
gokrusk <nigelljama@gmail.com>
2023-04-14 12:45:03 -0500

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className="mx-6">
+        <nav>
           <Header/>
           {/* <NavBar/> */}
         </nav>
